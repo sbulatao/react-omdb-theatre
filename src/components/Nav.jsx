b94140496ceceb0a27ef1e7661221fd4a0ab1acd
sbulatao <114209345+sbulatao@users.noreply.github.com>
2026-05-12 16:27:49 -0700

    Setup::NAV

---
+++ src/components/Nav.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from 'react-router';
+
+export default function Nav() {
+
+  return (
+    <nav>
+    <div className="navbar">
+        {/* NAVBAR LOGO */}
+        <div className="navbar__logo">
+            <FontAwesomeIcon icon="film" size='3x'></FontAwesomeIcon>
+            <div className="navbar__logo--title">OMDb Theater</div>
+        </div>
+
+            {/* LINKS */}
+            <ul className="navbar__links">
+                <li> {/* HOME */}
+                    <Link to="/" className="navbar__link link__hover-effect">Home</Link>
+                </li>
+
+                <li> {/* MOVIES */}
+                    <Link to="/movies" className="navbar__link link__hover-effect">Find Your Movies</Link>
+                </li>
+
+                <li> {/* CONTACT */}
+                    <Link to="/contact" className="button navbar__link--primary link__hover-effect" >Contact</Link>
+                </li>
+
+                <li> {/* ADDED: CART -- IN PROGRESS */}
+                    <Link to="/cart" className='navbar__link'>
+                        <FontAwesomeIcon icon="shopping-cart"></FontAwesomeIcon>
+                    </Link>
+                </li>
+
+                {/* LIGHT/DARK CONTRAST */}
+                <li className="navbar__link click link__hover-effect">
+                    <Link to="/" className="navbar__link--anchor"> 
+                        <FontAwesomeIcon icon="adjust"></FontAwesomeIcon>
+                    </Link>
+                </li>
+            </ul>
+
+            {/* <!-- menu button --> */}
+            <button className="button__menu">
+                <FontAwesomeIcon icon="bars" size='xl'></FontAwesomeIcon>
+            </button>
+
+        </div>
+    </nav>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './style.css';
+import './animate.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars,faAdjust, faFilm, faCartShopping, faTimes } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faBars, faAdjust, faFilm, faCartShopping, faTimes)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
