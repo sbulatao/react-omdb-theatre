1a1bb12bdbd15c0dfabdaa76143ba4f48acd30a1
sbulatao <114209345+sbulatao@users.noreply.github.com>
2026-05-12 16:29:26 -0700

    Setup::NAV

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from 'react-router';
 
 export default function Nav() {
-
   return (
     <nav>
     <div className="navbar">
