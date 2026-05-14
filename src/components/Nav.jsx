d2160ad7cf71b95fb1d3057d65f686d1d8efd2b0
sbulatao <114209345+sbulatao@users.noreply.github.com>
2026-05-13 23:22:12 -0700

    NAV::eopn/close menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,6 +3,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from 'react-router';
 
 export default function Nav() {
+    function openMenu() {
+        document.body.classList += " menu--open";
+    }
+
+    function closeMenu() {
+        document.body.classList.remove("menu--open");
+    }
   return (
     <nav>
         <div className="navbar">
@@ -46,9 +53,38 @@ export default function Nav() {
             </ul>
 
             {/* <!-- menu button --> */}
-            <button className="button__menu">
+            <button className="button__menu" onClick={openMenu}>
                 <FontAwesomeIcon icon="bars" size='xl'></FontAwesomeIcon>
             </button>
+
+            <div className="menu">
+                <button className="button__menu menu__close" onClick={closeMenu}>
+                    <FontAwesomeIcon icon="times"></FontAwesomeIcon>
+                </button>
+
+                <ul className="menu__links">
+                    <li className="menu__list">
+                        <Link to="/" className='menu__link'>Home</Link>
+                    </li>
+                    <li className="menu__list">
+                        <Link to="/movies" className='menu__link'>Find Your Movies</Link>
+                    </li>
+                    <li className="menu__list">
+                        <Link to="/login" className='menu__link'>Login</Link>
+                    </li>
+                    <li className="menu__list">
+                        <Link to="/contact" className='menu__link'>Contact</Link>
+                    </li>
+                    <li className="menu__list">
+                        <Link to="/cart" className='menu__link'>Cart</Link>
+                    </li>
+                    <li className="menu__list">
+                        <Link to="/" className="navbar__link--anchor"> 
+                            <FontAwesomeIcon icon="adjust"></FontAwesomeIcon>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
 
         </div>
     </nav>
