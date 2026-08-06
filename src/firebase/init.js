c29671bd681bf07fac83579e490f7ed9a2849cdc
sbulatao <114209345+sbulatao@users.noreply.github.com>
2026-08-06 16:09:09 -0700

    export app in init

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -18,6 +18,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const db = getFirestore(); // Create Read Update Delete stuff -- need data for database
