443db624a1f669bfe0b70c7ca19054870436e59f
sbulatao <114209345+sbulatao@users.noreply.github.com>
2026-09-18 00:09:32 -0700

    Created nav css and moved style to and from the css

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './Nav.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from 'react-router';
 import { auth } from '../firebase/init'; 
