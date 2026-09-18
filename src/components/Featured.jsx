da5618ada09a393e3a5733779b39502d4a8651c1
sbulatao <114209345+sbulatao@users.noreply.github.com>
2026-09-18 00:30:48 -0700

    Created featured css and moved style to and from .css

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
-// import Movie from './ui/Movie'
+import './Featured.css'
 
 export default function Featured() {
 
