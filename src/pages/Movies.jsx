77a34cc6f1a296ba4c54255f5a5f7239acee4904
sbulatao <114209345+sbulatao@users.noreply.github.com>
2026-09-18 00:58:59 -0700

    movies updated style css

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import './Movies.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Movie from '../components/ui/Movie'
 import Browse from '../components/Browse'
