bc21efe52d1ca723347caf66a5c2d73c940ffbd0
sbulatao <114209345+sbulatao@users.noreply.github.com>
2026-09-18 01:03:17 -0700

    updating moviesinfo style css

--- src/pages/MovieInfo.jsx
+++ src/pages/MovieInfo.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './MovieInfo.css';
 import { useParams, Link } from 'react-router'; // or 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import axios from 'axios';
