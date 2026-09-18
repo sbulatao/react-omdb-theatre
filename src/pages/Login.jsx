bd0b9123686cefec47f08dc9b54724d557cdaed7
sbulatao <114209345+sbulatao@users.noreply.github.com>
2026-09-18 00:35:48 -0700

    Updated style to and from css

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import './Login.css'
 import { auth } from '../firebase/init'
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth'
 
